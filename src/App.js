7c095d23c2a8922f893799219528d452bf56754d
oliverlangmo <oliver.langmo@gmail.com>
2016-12-30 13:46:18 -0600

    adds name form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id:'',
+      name:'',
+      answers:{
+      q1:'',
+      q2:'',
+      q3:'',
+      q4:''
+      },
+    submitted: false
+  }
+}
+handleNameSubmit(event){
+  var name = this.refs.name.value;
+this.setState({name:name}, function(){
+  console.log(this.state);
+});
+  event.preventDefault();
+}
   render() {
+    var user;
+    var questions;
+    if(this.state.name && this.state.submitted ===false){
+      user = <h2>Welcome {this.state.name}</h2>
+    } else if (!this.state.name && this.state.submitted ===false) {
+       user=<span>
+              <h2>Please Enter your name to begin the survey</h2>
+                <form onSubmit={this.handleNameSubmit.bind(this)}>
+                  <input type='text' placeholder='Please Enter Your Name' ref='name'></input>
+                </form>
+              </span>;
+        questions='';
+        } else if (this.state.submitted === true){
+
+        }
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Simple Survey</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='text-center'>
+          {user}
+        </div>
       </div>
     );
   }
